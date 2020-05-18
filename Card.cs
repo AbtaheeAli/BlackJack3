80c301c6ccc000945ffb20732e5729907af8e98b
Abtahee Ali <abtaheeali@gmail.com>
2020-05-18 17:22:56 -0400

    Added class for Hand

---
+++ Card.cs
@@ -0,0 +1,65 @@
+namespace BlackJack3
+{
+    class Card
+    {
+        public string Rank { get; set; }
+        public string Suit { get; set; }
+        public int Value()
+        {
+            //Takes the string of rank and turns it into an int
+            if (Rank == "Ace")
+            {
+                return 11;
+            }
+            if (Rank == "King")
+            {
+                return 10;
+            }
+            if (Rank == "Queen")
+            {
+                return 10;
+            }
+            if (Rank == "Jack")
+            {
+                return 10;
+            }
+            if (Rank == "Ten")
+            {
+                return 10;
+            }
+            if (Rank == "Nine")
+            {
+                return 9;
+            }
+            if (Rank == "Eight")
+            {
+                return 8;
+            }
+            if (Rank == "Seven")
+            {
+                return 7;
+            }
+            if (Rank == "Six")
+            {
+                return 6;
+            }
+            if (Rank == "Five")
+            {
+                return 5;
+            }
+            if (Rank == "Four")
+            {
+                return 4;
+            }
+            if (Rank == "Three")
+            {
+                return 3;
+            }
+            if (Rank == "Two")
+            {
+                return 2;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Hand.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace BlackJack3
+{
+    class Hand
+    {
+        public List<Card> CardsHeld = new List<Card>();
+
+        public void Accept(Card cardWeAreHolding)
+        {
+            CardsHeld.Add(cardWeAreHolding);
+        }
+        public void ShowTheCards()
+        {
+            foreach (var card in CardsHeld)
+                Console.WriteLine($"{card.Rank} of {card.Suit}");
+
+        }
+        public int TotalValue()
+        {
+            var total = 0;
+
+            foreach (var card in CardsHeld)
+
+            {
+                var cardValue = card.Value();
+
+                total += cardValue;
+
+            }
+
+            return total;
+
+        }
+    }
+}
